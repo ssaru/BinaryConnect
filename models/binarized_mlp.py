4322b8ebceae5f0289aaf1ab57dd5a2a6e9afc26
Martin Hwang <dhhwang89@gmail.com>
2020-04-15 19:37:45 +0000

    feat: solve merge conflict
     #39

--- models/binarized_mlp.py
+++ models/binarized_mlp.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 import torch
 import torch.nn as nn
@@ -6,18 +5,11 @@ import torch.optim as optim
 
 from torchsummary import summary
 from layers.binarized_linear_layer import BinarizedLinear
-=======
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torchsummary import summary
-from layers.binarized_linear import BinarizedLinear
->>>>>>> bfde0aa1aff9e990c2606cafdb25ebbdf40aa0df
 
 
 class Binarized_MLP(nn.Module):
     def __init__(self,
-                 mode: str = "determistic",
+                 mode: str = "stochastic",
                  is_dropout: bool = False,
                  dropout_prob: float = 0.5,
                  optimizer: optim = optim.SGD,
@@ -37,9 +29,7 @@ class Binarized_MLP(nn.Module):
         self.batch3 = nn.BatchNorm1d(1024)
         self.fc4 = BinarizedLinear(1024, 10, bias=False, mode=mode)
 
-        # Activation
         self.relu = nn.ReLU()
-        self.sigmoid = nn.Sigmoid()
 
         # Dropout
         self.is_dropout = is_dropout
@@ -54,22 +44,6 @@ class Binarized_MLP(nn.Module):
                                momentum=momentum,
                                weight_decay=weight_decay)
 
-<<<<<<< HEAD
-        # Dropout
-        self.is_dropout = is_dropout
-        if self.is_dropout:
-            self.dropout1 = nn.Dropout(dropout_prob)
-            self.dropout2 = nn.Dropout(dropout_prob)
-            self.dropout3 = nn.Dropout(dropout_prob)
-
-        # Optimizer
-        self.optim = optimizer(self.parameters(),
-                               lr=learning_rate,
-                               momentum=momentum,
-                               weight_decay=weight_decay)
-
-=======
->>>>>>> bfde0aa1aff9e990c2606cafdb25ebbdf40aa0df
         # Optimizer Scheduler
         if scheduler:
             self.scheduler = scheduler(self.optim, scheduler_gamma)
@@ -98,7 +72,6 @@ class Binarized_MLP(nn.Module):
             x = self.dropout3(x)
 
         x = self.fc4(x)
-        x = self.sigmoid(x)
 
         return x
 
@@ -128,12 +101,3 @@ class Binarized_MLP(nn.Module):
         tp: int = pred.eq(target.data.view_as(pred)).sum()
 
         return tp
-<<<<<<< HEAD
-=======
-
-
-if __name__ == "__main__":
-
-    model = Binarized_MLP()
-    model.weight_clipping()
->>>>>>> bfde0aa1aff9e990c2606cafdb25ebbdf40aa0df
